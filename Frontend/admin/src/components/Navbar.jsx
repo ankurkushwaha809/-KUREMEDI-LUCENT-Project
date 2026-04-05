262f228e16a00fbdf5fbf666b928142626ff0973
ankurkushwaha809 <ankurkushwaha809@gmail.com>
2026-04-05 14:29:34 +0530

    KUREMEDI LUCENT bio

--- Frontend/admin/src/components/Navbar.jsx
+++ Frontend/admin/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import { useContextApi } from "../hooks/useContextApi";
 
 const Navbar = ({ className = "" }) => {
-    const loggedInSalesName = "KUREMEDI LUCENT bio 2";
+    const loggedInSalesName = "KUREMEDI LUCENT bio 3";
     const navigate = useNavigate();
     const [, setSearchParams] = useSearchParams();
     const { setActiveTab, getProducts } = useContextApi();
