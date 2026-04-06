db326e5399eda2ead86a17059ae655e4ffb1c0b3
ankurkushwaha809 <ankurkushwaha809@gmail.com>
2026-04-07 01:03:36 +0530

    setup frontend CI/CD

--- Frontend/website/src/components/Header.jsx
+++ Frontend/website/src/components/Header.jsx
@@ -94,7 +94,7 @@ export default function Header() {
                                     </span> */}
                                 </span>
                             </button>
-                        </div>
+                        </div> 
 
                         <div className="flex items-center gap-3 md:gap-5">
 
