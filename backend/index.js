9af6e6372012f7da6e67e6686615d08c788575cf
ankurkushwaha809 <ankurkushwaha809@gmail.com>
2026-04-07 10:08:25 +0530

    setup backend CI/CD

--- backend/index.js
+++ backend/index.js
@@ -55,7 +55,7 @@ app.use(express.json());
 
 // Test Route
 app.get("/", (req, res) => {
-  res.send("CI/CD deployed successfully hehe🚀");
+  res.send("CI/CD deployed successfully hehe🚀 22");
 });
 // app.get("/", (req, res) => {
 //   res.send("Server is running  on port 5000🚀");
