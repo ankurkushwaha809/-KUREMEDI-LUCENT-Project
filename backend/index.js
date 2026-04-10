b81e4bc17c158a6d843dd80b29ac24ab28e7340e
ankurkushwaha809 <ankurkushwaha809@gmail.com>
2026-04-10 13:52:01 +0530

    check backend cicd

--- backend/index.js
+++ backend/index.js
@@ -59,7 +59,7 @@ app.use(express.json());
 
 // Test Route
 app.get("/", (req, res) => {
-  res.send("CI/CD deployed successfully");
+  res.send("CI/CD deployed successfully on Railway! 🚀");
 });
 // app.get("/", (req, res) => {
 //   res.send("Server is running  on port 5000🚀");
